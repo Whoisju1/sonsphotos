86fb3f3ace32668052945576d889da3663f32204
Juan John-Charles <whoisju1@gmail.com>
2018-08-28 07:19:16 -0400

    remove boilerplace html stuff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: .5px solid black;
+  display: grid;
+  min-width: 100vw;
+  height: 100vh;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        Sons Photos Homepage
+      </Container>
     );
   }
 }
