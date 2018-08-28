f5edebe45aeb3b821ab7e1ea56720be58a3599a8
Juan John-Charles <whoisju1@gmail.com>
2018-08-28 19:12:55 -0400

    add themeProvider

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import Main from "./components/Main";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { ThemeProvider, injectGlobal } from 'styled-components';
+
+injectGlobal`
+
+`;
+
+const theme = {
+  color: {
+    primary: '#ff4d4d',
+  }
+};
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <Main>
-          Some Content
-        </Main>
-        <Footer />
-      </React.Fragment>
+        <ThemeProvider theme={theme}>
+          <React.Fragment>
+              <Header />
+              <Main>
+                Some Content
+              </Main>
+              <Footer />
+          </React.Fragment>
+        </ThemeProvider>
     );
   }
 }
