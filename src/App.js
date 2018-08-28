f6795a29e421b926fc3f4a8f0c6328f790e2c688
Juan John-Charles <whoisju1@gmail.com>
2018-08-28 18:58:47 -0400

    remove unused variable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import Main from "./components/Main";
 import Header from './components/Header';
 import Footer from './components/Footer';
