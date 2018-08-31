e272dcbe4e2760a65e2addf6f4d28cbe4bb5db6b
Juan John-Charles <whoisju1@gmail.com>
2018-08-31 05:39:54 -0400

    center everything in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
+  display: grid;
+  justify-content: center;
+  align-items: center;
   grid-column: 1/-1;
   grid-row: footer-start/footer-end;
 `;
