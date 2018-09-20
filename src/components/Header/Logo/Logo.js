e4b4e576c8789faa9a4611bf9de9127b67f02f36
Juan John-Charles <whoisju1@gmail.com>
2018-09-19 21:27:40 -0400

    change font and color of text in logo

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LogoText= styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Cinzel');
+  font-family: 'Cinzel', serif;
   grid-column: 2/3;
   display: grid;
-  color: #fff;
+  color: #c8d6e5;
+  font-weight: 100;
   justify-content: center;
   align-items: center;
   letter-spacing: .4rem;
